4d2af616ab71cfc0d02b03c03e36fa2783ed6822
Mike West <micwest@ebay.com>
2013-09-12 17:12:32 -0400

    product_image continued work on Image Feed tests, added yaml for vfs containing a sample XML feed

--- src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/Image/MasterTest.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Test/Model/Feed/Image/MasterTest.php
@@ -5,7 +5,8 @@
 class TrueAction_Eb2cProduct_Test_Model_Feed_Image_MasterTest extends TrueAction_Eb2cCore_Test_Base
 {
 	const MAGE_MODEL_NAME = 'eb2cproduct/feed_image_master';
-	const VFS_ROOT		= 'imageFeedTestBase';
+	const VFS_ROOT        = 'testImageRoot';
+
 	/**
 	 * Instantiate the Model, test we get the expected model back
 	 *
@@ -33,41 +34,19 @@ class TrueAction_Eb2cProduct_Test_Model_Feed_Image_MasterTest extends TrueAction
 	}
 
 	/**
-	 * Instantiate the Model, test we get return code of 0 back
-	 *
-	 * @test
-	 */
-	public function testReturnsZero()
-	{
-		$this->assertSame(
-			0,
-			Mage::getModel('eb2cproduct/feed_image_master')->processFeeds()
-		);
-	}
-
-
-	/**
 	 * Test with a mock'd up filesystem
+	 * 
+	 * @test
+	 * @loadFixture
 	 */
 	public function testFeedProcess()
 	{
 		$vfs = $this->getFixture()->getVfs();
-		$vfs->apply(
-			array(
-				self::VFS_ROOT => array (
-					TrueAction_Eb2cCore_Model_Feed::INBOUND_DIR_NAME  => array(),
-					TrueAction_Eb2cCore_Model_Feed::OUTBOUND_DIR_NAME => array(),
-					TrueAction_Eb2cCore_Model_Feed::ARCHIVE_DIR_NAME  => array(),
-					TrueAction_Eb2cCore_Model_Feed::ERROR_DIR_NAME    => array(),
-					TrueAction_Eb2cCore_Model_Feed::TMP_DIR_NAME      => array(),
-				)
-			)
-		);
 
 		// Set up a Varien_Io_File style array for dummy file listing.
 		$vfsDump = $vfs->dump();
 		$dummyFiles = array ();
-		foreach( $vfsDump['root'][self::VFS_ROOT]['inbound'] as $filename => $contents ) {
+		foreach( $vfsDump['root'][self::VFS_ROOT][TrueAction_Eb2cCore_Model_Feed::INBOUND_DIR_NAME] as $filename => $contents ) {
 			$dummyFiles[] = array('text' => $filename, 'filetype' => 'xml');
 		}
 
@@ -109,20 +88,22 @@ class TrueAction_Eb2cProduct_Test_Model_Feed_Image_MasterTest extends TrueAction
 			->with($this->logicalOr($this->identicalTo(true), $this->identicalTo(false)))
 			->will($this->returnSelf());
 
-		// @todo: Mock the transport  - we just pretend we got files
-		// $this->replaceModel('filetransfer/protocol_types_sftp', array('getFile' => true,));
-
-		// @todo: Mock the core config registry to know where our local files are to process
-		/*
-		$this->replaceCoreConfigRegistry(
-			array (
-				'feedLocalPath' => $vfs->url(self::VFS_ROOT),
+		// Mock the transport  - to just pretend we got files
+		$this->replaceByMock(
+			'model',
+			'filetransfer/protocol_types_sftp',
+			$this->_buildModelMock(
+				'filetransfer/protocol_types_sftp',
+				array(
+					'getFile' => $this->returnValue(true),
+				)
 			)
 		);
-	 	*/
+
+		// @todo: Mock the core config registry to know where our local files are to process
 
 		$this->assertSame(
-			0,
+			1,
 			Mage::getModel(
 				'eb2cproduct/feed_image_master',
 				array(
