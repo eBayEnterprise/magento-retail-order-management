1eff76b98581ca5c43b98ff6e3b5020f78e458ea
Mike West <micwest@ebay.com>
2013-09-19 11:42:18 -0400

    product_image Having moved Abstract into Core, we delete it from here. Also, we make Master extend the core version.

--- src/app/code/local/TrueAction/Eb2cProduct/Model/Feed/Image/Master.php
+++ src/app/code/local/TrueAction/Eb2cProduct/Model/Feed/Image/Master.php
@@ -7,7 +7,7 @@
  * @todo: Finish this Feed
  */
 class TrueAction_Eb2cProduct_Model_Feed_Image_Master
-	extends TrueAction_Eb2cProduct_Model_Feed_Image_Abstract
+	extends TrueAction_Eb2cCore_Model_Feed_Abstract
 	implements TrueAction_Eb2cCore_Model_Feed_Interface
 {
 	private $_imageAttrs = array('imagewidth', 'imageview', 'imageurl', 'imagename', 'imageheight');
